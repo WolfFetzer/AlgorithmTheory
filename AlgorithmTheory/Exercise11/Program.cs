f2efbefd18880fe04b449b26e39dc42d0803c59c
Wolf Fetzer <wfetzer@stud.hs-heilbronn.de>
2022-12-15 13:30:16 +0100

    FibHeap maybe with bugs

---
+++ AlgorithmTheory/Exercise11/Program.cs
@@ -0,0 +1,333 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise11
+{
+    /// <summary>
+    /// Fibonacci Heap
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fh = new FibonacciHeap();
+            
+            fh.PrintRow();
+            
+            fh.Insert(1);
+            fh.PrintRow();
+            
+            fh.Insert(3);
+            fh.PrintRow();
+            
+            fh.Insert(6);
+            fh.PrintRow();
+
+            var fh2 = new FibonacciHeap();
+            fh2.PrintRow();
+
+            fh2.Insert(0);
+            fh2.PrintRow();
+
+            fh2.Insert(10);
+            fh2.PrintRow();
+            
+            fh2.Insert(12);
+            fh2.PrintRow();
+            
+            fh.Merge(fh2);
+            fh.PrintRow();
+
+            fh.DeleteMin();
+            fh.PrintRow();
+            
+            fh.DecreaseKey(fh.Min, 200);
+            fh.PrintRow();
+        }
+    }
+
+    public class FibonacciHeap
+    {
+        public FhNode Min { get; private set; }
+        
+        public void Insert(int priority)
+        {
+            FhNode node = new FhNode(priority);
+            if (IsEmpty())
+            {
+                Min = node;
+                return;
+            }
+
+            Insert(node);
+            CheckMin(node);
+        }
+        
+        public void DeleteMin()
+        {
+            Remove(Min);
+            if (Min.right == Min)
+            {
+                Min = null;
+                return;
+            }
+            
+            Min = Min.right;
+            FhNode currentNode = Min.right;
+            FhNode startingNode = Min;
+
+            Dictionary<int, FhNode> rankDictionary = new Dictionary<int, FhNode>();
+            rankDictionary.Add(Min.Rank, Min);
+
+            while (currentNode != startingNode)
+            {
+                CheckMin(currentNode);
+                
+                //Link trees
+                bool b = true;
+                while (b)
+                {
+                    if (rankDictionary.ContainsKey(currentNode.Rank))
+                    {
+                        int rank = currentNode.Rank;
+                        currentNode = Link(currentNode, rankDictionary[rank]);
+                        startingNode = currentNode;
+                        rankDictionary.Remove(rank);
+                        
+                    }
+                    else
+                    {
+                        rankDictionary.Add(currentNode.Rank, currentNode);
+                        b = false;
+                    }
+                }
+                
+                currentNode = currentNode.right;
+            }
+        }
+        
+        public void Delete(FhNode node)
+        {
+            if (node == Min) DeleteMin();
+            else Remove(node);
+        }
+
+        public void DecreaseKey(FhNode node, int priority)
+        {
+            node.priority = priority;
+            if (node == Min)
+            {
+                FindMin();
+                return;
+            }
+            
+            if (node.parent == null || node.parent < node) return;
+            if (node.parent.child == node)
+            {
+                if (node.right != node)
+                {
+                    UpdateParent(node);
+                    UnlinkFromRow(node);
+                }
+            }
+            
+            Insert(node);
+        }
+
+        private void FindMin()
+        {
+            FhNode currentMin = Min;
+            FhNode startingMin = Min;
+            
+            do
+            {
+                CheckMin(currentMin);
+                currentMin = currentMin.right;
+            } while (currentMin != startingMin);
+        }
+
+        public void Merge(FibonacciHeap heapToAdd)
+        {
+            if(heapToAdd.IsEmpty()) return;
+            if (IsEmpty())
+            {
+                Min = heapToAdd.Min;
+                return;
+            }
+            
+            Insert(heapToAdd.Min);
+            CheckMin(heapToAdd.Min);
+        }
+
+        private FhNode Link(FhNode node1, FhNode node2)
+        {
+            FhNode parent = node1;
+            FhNode child = node2;
+            if (node2 < node1)
+            {
+                parent = node2;
+                child = node1;
+            }
+
+            UnlinkFromRow(child);
+            
+            if (parent.child != null)
+            {
+                parent.child.left.right = child;
+                child.left = parent.child.left;
+                parent.child.left = child;
+                child.right = parent.child;
+            }
+            else
+            {
+                parent.child = child;
+                child.left = child;
+                child.right = child;
+            }
+            
+            child.parent = parent;
+            
+            parent.Rank++;
+            return parent;
+        }
+        
+        private void Insert(FhNode node)
+        {
+            node.left.right = Min.right;
+            Min.right.left = node.left;
+            node.left = Min;
+            Min.right = node;
+        }
+        
+        private void Remove(FhNode node)
+        {
+            RemoveChildren(node);
+            UpdateParent(node);
+            UnlinkFromRow(node);
+        }
+
+        private void UpdateParent(FhNode node)
+        {
+            if (node.parent == null) return;
+            node.parent.Marked = true;
+            node.parent.Rank--;
+            if (node.parent.child == node)
+            {
+                if (node.right != node) node.parent.child = node.right;
+                else node.parent.child = null;
+            }
+        }
+
+        private void RemoveChildren(FhNode node)
+        {
+            if (node.child == null) return;
+            Insert(node.child);
+            node.child = null;
+        }
+
+        private void UnlinkFromRow(FhNode node)
+        {
+            node.left.right = node.right;
+            node.right.left = node.left;
+        }
+        
+        private void CheckMin(FhNode nodeToCheck)
+        {
+            if (nodeToCheck < Min) Min = nodeToCheck;
+        }
+
+        public bool IsEmpty()
+        {
+            return Min == null;
+        }
+
+        public void PrintRow(FhNode node = null)
+        {
+            if (node == null)
+            {
+                Console.WriteLine("New Print ------------------------------------------");
+                node = Min;
+            }
+            FhNode currentNode = node;
+            node = currentNode;
+            
+            if (IsEmpty())
+            {
+                Console.WriteLine("Empty");
+                return;
+            }
+            
+            string s = string.Empty;
+
+            do
+            {
+                string parent = currentNode.parent == null ? string.Empty : $" (Parent {currentNode.parent})";
+                s += $"{currentNode}{parent} -> ";
+
+                if (currentNode.child != null) PrintRow(currentNode.child);
+                
+                
+                currentNode = currentNode.right;
+            } while (currentNode != node);
+
+            Console.WriteLine(s);
+        }
+    }
+
+    public class FhNode
+    {
+        public FhNode child, parent, left, right;
+        /// <summary> Explanation: Number of children </summary>
+        public int Rank { get; set; }
+
+        private bool _marked;
+        public bool Marked
+        {
+            get => _marked;
+            set
+            {
+                if (Marked && value)
+                {
+                    Console.WriteLine($"{priority} is two times marked");
+                }
+                else
+                {
+                    _marked = value;
+                }
+            }
+        }
+
+        public int priority;
+
+        public FhNode(int priority)
+        {
+            left = right = this;
+            this.priority = priority;
+        }
+
+        public override string ToString()
+        {
+            return priority.ToString();
+        }
+
+        public static bool operator <(FhNode self, FhNode other)
+        {
+            return self.priority < other.priority;
+        }
+
+        public static bool operator >(FhNode self, FhNode other)
+        {
+            return self.priority > other.priority;
+        }
+
+        public static bool operator <=(FhNode self, FhNode other)
+        {
+            return self.priority <= other.priority;
+        }
+
+        public static bool operator >=(FhNode self, FhNode other)
+        {
+            return self.priority >= other.priority;
+        }
+    }
+}
--- AlgorithmTheory/Exercise8/Program.cs
+++ AlgorithmTheory/Exercise8/Program.cs
@@ -2,112 +2,192 @@
 
 namespace Exercise8
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
-
-    /**
- * Class DLXNode
- *   represents a matrix element of the cover matrix with value 1
- *   links go to up down left right neigbors, and column header
- *   can also be used as colm header or root of column headers
- *   matrix is sparsely coded
- *   try to do all operations very efficiently
- *   see:
- *      http://en.wikipedia.org/wiki/Dancing_Links
- *      http://arxiv.org/abs/cs/0011047 
- */
+    /// <summary>
+    /// represents a matrix element of the cover matrix with value
+    /// links go to up down left right neighbors, and column header
+    /// can also be used as colm header or root of column headers
+    /// matrix is sparsely coded
+    /// try to do all operations very efficiently
+    /// see:
+    /// http://en.wikipedia.org/wiki/Dancing_Links
+    /// http://arxiv.org/abs/cs/0011047 
+    /// </summary>
     class DLXNode
     {
         // represents 1 element or header
-        DLXNode C; // reference to column-header
-        DLXNode L, R, U, D; // left, right, up, down references
+        public DLXNode ColumnHeader; // reference to column-header
+        public DLXNode Left, Right, Up, Down; // left, right, up, down references
+        public string Name;
 
-        DLXNode()
+        public DLXNode()
         {
-            C = L = R = U = D = this;
+            ColumnHeader = Left = Right = Up = Down = this;
         } // supports circular lists
+
+        /// <summary>
+        /// search tries to find and count all complete coverings of the DLX matrix.
+        /// Is a recursive, depth-first, backtracking algorithm that finds
+        /// all solutions to the exact cover problem encoded in the DLX matrix.
+        /// each time all columns are covered, static long cnt is increased
+        /// <param name="k">number of level</param>
+        /// </summary>
+        public static void Search(DLXNode matrixHeader, int k, ref int counter)
+        {
+            // finds & counts solutions 
+            if (matrixHeader.Right == matrixHeader)
+            {
+                counter++;
+                return;
+            } // if empty: count & done
+
+            DLXNode c = matrixHeader.Right; // choose next column c
+            Cover(c); // remove c from columns
+            for (DLXNode r = c.Down; r != c; r = r.Down)
+            {
+                // forall rows with 1 in c
+                for (DLXNode j = r.Right; j != r; j = j.Right) // forall 1-elements in row
+                    Cover(j.ColumnHeader); // remove column
+                Search(matrixHeader, k + 1, ref counter); // recursion
+                for (DLXNode j = r.Left; j != r; j = j.Left) // forall 1-elements in row
+                    Uncover(j.ColumnHeader); // backtrack: un-remove
+            }
+
+            Uncover(c); // un-remove c to columns
+        }
+
+        /// <summary>
+        /// cover "covers" a column c of the DLX matrix
+        /// column c will no longer be found in the column list
+        ///     rows i with 1 element in column c will no longer be found
+        ///     in other column lists than c
+        ///     so column c and rows i are invisible after execution of cover
+        /// <param name="c">header element of column that has to be covered</param>
+        /// </summary>
+        public static void Cover(DLXNode c)
+        {
+            // remove column c
+            c.Right.Left = c.Left; // remove header  
+            c.Left.Right = c.Right; // .. from row list
+            for (DLXNode i = c.Down; i != c; i = i.Down) // forall rows with 1
+            for (DLXNode j = i.Right; i != j; j = j.Right)
+            {
+                // forall elem in row
+                j.Down.Up = j.Up; // remove row element
+                j.Up.Down = j.Down; // .. from column list
+            }
+        }
+
+        ///<summary>
+        /// uncover "uncovers" a column c of the DLX matrix
+        ///       all operations of cover are undone
+        ///       so column c and rows i are visible again after execution of uncover
+        /// <param name="c">header element of column that has to be uncovered</param>
+        /// </summary>
+        public static void Uncover(DLXNode c)
+        {
+            //undo remove col c
+            for (DLXNode i = c.Up; i != c; i = i.Up) // forall rows with 1
+            for (DLXNode j = i.Left; i != j; j = j.Left)
+            {
+                // forall elem in row
+                j.Down.Up = j; // un-remove row elem
+                j.Up.Down = j; // .. to column list
+            }
+
+            c.Right.Left = c; // un-remove header
+            c.Left.Right = c; // .. to row list
+        }
     }
 
-    /*
-     * search tries to find and count all complete coverings of the DLX matrix.
-     *        Is a recursive, depth-first, backtracking algorithm that finds
-     *        all solutions to the exact cover problem encoded in the DLX matrix.
-     *        each time all columns are covered, static long cnt is increased
-     * @param int k: number of level
-     *
-     */
-    /*
-    public static void search(int k)
+    class Program
     {
-        // finds & counts solutions 
-        if (h.R == h)
+        private static void Main(string[] args)
         {
-            cnt++;
+            int number = 4;
+
+            DLXNode[] nodeHeaders = new DLXNode[number * 5];
+            
+            var header = new DLXNode() {Name = "H"};
+            DLXNode current = header;
+            
+            for (int i = 0; i < number * 5; i++)
+            {
+                DLXNode node = new DLXNode() {Left = current, Right = header, Name = $"C{i}"};
+                current.Right = node;
+                current = node;
+                header.Left = node;
+
+                nodeHeaders[i] = node;
+            }
+            
+            int distance = 1;
+            int maxElement = 5 * number;
+            
+            while (distance < number + 1)
+            {
+                int position = 0;
+                while (position + 4 * distance < maxElement)
+                {
+                    DLXNode[] nodes = new DLXNode[5];
+                    
+                    nodes[0] = new DLXNode() { ColumnHeader = nodeHeaders[position], Name = $"{position}"};
+                    nodes[1] = new DLXNode() { ColumnHeader = nodeHeaders[position + distance], Name = $"{position + distance}" };
+                    nodes[2] = new DLXNode() { ColumnHeader = nodeHeaders[position + 2 * distance], Name = $"{position + distance}" };
+                    nodes[3] = new DLXNode() { ColumnHeader = nodeHeaders[position + 3 * distance], Name = $"{position + distance}" };
+                    nodes[4] = new DLXNode() { ColumnHeader = nodeHeaders[position + 4 * distance], Name = $"{position + distance}" };
+                    
+                    nodes[0].ColumnHeader.Up.Down = nodes[0];
+                    nodes[1].ColumnHeader.Up.Down = nodes[1];
+                    nodes[2].ColumnHeader.Up.Down = nodes[2];
+                    nodes[3].ColumnHeader.Up.Down = nodes[3];
+                    nodes[4].ColumnHeader.Up.Down = nodes[4];
+
+                    nodes[0].ColumnHeader.Up = nodes[0];
+                    nodes[1].ColumnHeader.Up = nodes[1];
+                    nodes[2].ColumnHeader.Up = nodes[2];
+                    nodes[3].ColumnHeader.Up = nodes[3];
+                    nodes[4].ColumnHeader.Up = nodes[4];
+                    
+                    nodes[0].Left = nodes[4];
+                    nodes[1].Left = nodes[0];
+                    nodes[2].Left = nodes[1];
+                    nodes[3].Left = nodes[2];
+                    nodes[4].Left = nodes[3];
+                        
+                    nodes[0].Right = nodes[1];
+                    nodes[1].Right = nodes[2];
+                    nodes[2].Right = nodes[3];
+                    nodes[3].Right = nodes[4];
+                    nodes[4].Right = nodes[0];
+
+                    position++;
+                }
+
+                distance++;
+            }
+
+            DLXNode cNode = header;
+            DLXNode rNode;
+            string s = String.Empty;
+            do
+            {
+                s += cNode.Name + " ";
+                cNode = cNode.Right;
+                rNode = cNode.Down;
+                while (rNode != cNode)
+                {
+                    Console.WriteLine(rNode.Name);
+                    rNode = rNode.Down;
+                }
+
+            } while (cNode != header);
+
+            Console.WriteLine(s);
             return;
-        } // if empty: count & done
+            int counter = 0;
+            DLXNode.Search(header, 0, ref counter);
 
-        DLXNode c = h.R; // choose next column c
-        cover(c); // remove c from columns
-        for (DLXNode r = c.D; r != c; r = r.D)
-        {
-            // forall rows with 1 in c
-            for (DLXNode j = r.R; j != r; j = j.R) // forall 1-elements in row
-                cover(j.C); // remove column
-            search(k + 1); // recursion
-            for (DLXNode j = r.L; j != r; j = j.L) // forall 1-elements in row
-                uncover(j.C); // backtrack: un-remove
-        }
-
-        uncover(c); // un-remove c to columns
-    }
-
-    /**
-     * cover "covers" a column c of the DLX matrix
-     *       column c will no longer be found in the column list
-     *       rows i with 1 element in column c will no longer be found
-     *       in other column lists than c
-     *       so column c and rows i are invisible after execution of cover
-     * @param DLXNode c: header element of column that has to be covered
-     *
-     */
-    /* public static void cover(DLXNode c)
-    {
-        // remove column c
-        c.R.L = c.L; // remove header  
-        c.L.R = c.R; // .. from row list
-        for (DLXNode i = c.D; i != c; i = i.D) // forall rows with 1
-        for (DLXNode j = i.R; i != j; j = j.R)
-        {
-            // forall elem in row
-            j.D.U = j.U; // remove row element
-            j.U.D = j.D; // .. from column list
+            Console.WriteLine(counter);
         }
     }
-
-    /**
-     * uncover "uncovers" a column c of the DLX matrix
-     *       all operations of cover are undone
-     *       so column c and rows i are visible again after execution of uncover
-     * @param DLXNode c: header element of column that has to be uncovered
-     *
-     */
-    /* public static void uncover(DLXNode c)
-    {
-        //undo remove col c
-        for (DLXNode i = c.U; i != c; i = i.U) // forall rows with 1
-        for (DLXNode j = i.L; i != j; j = j.L)
-        {
-            // forall elem in row
-            j.D.U = j; // un-remove row elem
-            j.U.D = j; // .. to column list
-        }
-
-        c.R.L = c; // un-remove header
-        c.L.R = c; // .. to row list
-    }*/
 }
